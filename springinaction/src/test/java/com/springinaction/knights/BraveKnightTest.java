29702c8223ad140c6c98d3dbe4e0689daa38abde
Jzzza <jzzza1@gmail.com>
2020-05-16 15:42:09 +0300

    Added mockMinstrel for run

--- springinaction/src/test/java/com/springinaction/knights/BraveKnightTest.java
+++ springinaction/src/test/java/com/springinaction/knights/BraveKnightTest.java
@@ -8,8 +8,9 @@ class BraveKnightTest{
     @Test
     public void knightShouldEmbarkOnQuest() throws QuestException{
         Quest mockQuest = mock(Quest.class); // Создание фиктивного объекта Quest
+        Minstrel mockMinstrel = mock(Minstrel.class);
 
-        BraveKnight knight = new BraveKnight(mockQuest); // Внедрение
+        BraveKnight knight = new BraveKnight(mockQuest, mockMinstrel); // Внедрение
         knight.embarkOnQuest();
 
         verify(mockQuest, times(1)).embark();
