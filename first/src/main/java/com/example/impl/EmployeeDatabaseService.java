cab835e407f537b4401c71c14402f4b733dd9442
Jzzza <jzzza1@gmail.com>
2020-05-16 12:35:24 +0300

    Use jdbcTemplate for dont write repeated part of code but using general solution (p. 48)

--- first/src/main/java/com/example/impl/EmployeeDatabaseService.java
+++ first/src/main/java/com/example/impl/EmployeeDatabaseService.java
@@ -7,64 +7,31 @@
  */
 
 package com.example.impl;
+
 import com.example.DatabaseService;
 import com.example.Employee;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class EmployeeDatabaseService implements DatabaseService {
     private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
     Employee getEmployeeById(long id) {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            stmt = conn.prepareStatement("select id, firstname, lastname, salary from" +
-                    "employee where id=?");
-            stmt.setLong(1, id);
-            rs = stmt.executeQuery();
-            Employee employee = null;
-            if (rs.next()) {
-                employee = new Employee();
+        return jdbcTemplate.queryForObject("select id, firstname, lastname, salary from" +
+                "employee where id=?", new RowMapper<Employee>() {
+            public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Employee employee = new Employee();
                 employee.setId(rs.getLong("id"));
                 employee.setFirstName(rs.getString("firstname"));
                 employee.setLastName(rs.getString("lastname"));
                 employee.setSalary(rs.getBigDecimal("salary"));
+                return employee;
             }
-            return employee;
-        } catch (SQLException throwables) { // Что здесь следует делать?
-            throwables.printStackTrace();
-        } finally {
-            if (rs != null) {
-                try{
-                    rs.close();
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-
-            if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-        }
-        return null;
+        });
     }
 }
