61fa6b9d55961bc99862d3eee9896f82dd1a5eba
Jzzza <jzzza1@gmail.com>
2020-05-16 10:31:17 +0300

    Example with useless methods via EJB 2.1. (p. 33)

--- first/src/main/java/com/habuma/ejb/session/HelloWorldBean.java
+++ first/src/main/java/com/habuma/ejb/session/HelloWorldBean.java
@@ -1,7 +1,32 @@
 package com.habuma.ejb.session;
 
+import javax.ejb.EJBException;
 import javax.ejb.SessionBean;
-import java.ejb.
+import javax.ejb.SessionContext;
+import java.rmi.RemoteException;
 
-public class HelloWorldBean {
+public class HelloWorldBean implements SessionBean{ // Зачем все эти методы
+    public void setSessionContext(SessionContext sessionContext) throws EJBException, RemoteException {
+
+    }
+
+    public void ejbPassivate() throws EJBException, RemoteException {
+
+    }
+
+    public void ejbRemove() throws EJBException, RemoteException {
+
+    }
+
+    public void ejbActivate(){
+
+    }
+
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    public void ejbCreate(){
+
+    }
 }
