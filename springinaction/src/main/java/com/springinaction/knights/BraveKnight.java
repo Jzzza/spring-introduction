24e0ca8420e9ab59227dd1ed775e6268dec8075e
Jzzza <jzzza1@gmail.com>
2020-05-16 11:48:21 +0300

    Need to manage minstrel (p. 44)

--- springinaction/src/main/java/com/springinaction/knights/BraveKnight.java
+++ springinaction/src/main/java/com/springinaction/knights/BraveKnight.java
@@ -2,13 +2,17 @@ package com.springinaction.knights;
 
 public class BraveKnight implements Knight {
     private Quest quest;
+    private Minstrel minstrel;
 
     // Пример внедрения зависмости через конструктор
-    public BraveKnight(Quest quest){
+    public BraveKnight(Quest quest, Minstrel minstrel){
         this.quest = quest; // Внедрение сценария подвига
+        this.minstrel = minstrel;
     }
 
     public void  embarkOnQuest() throws QuestException {
+        minstrel.singBeforeQuest(); // Должен ли рыцарь руководить своим менестрелем?
         quest.embark();
+        minstrel.singAfterQuest();
     }
 }
