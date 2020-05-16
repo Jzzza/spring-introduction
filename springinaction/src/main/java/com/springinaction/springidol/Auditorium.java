5a5a68260e3f7701d63e92e8977d3e5aacd6a5a6
Jzzza <jzzza1@gmail.com>
2020-05-16 16:52:26 +0300

    Implemented methods for test default config (p. 84)

--- springinaction/src/main/java/com/springinaction/springidol/Auditorium.java
+++ springinaction/src/main/java/com/springinaction/springidol/Auditorium.java
@@ -2,10 +2,10 @@ package com.springinaction.springidol;
 
 public class Auditorium {
     public void turnOnLights(){
-
+        System.out.println("turnOnLights");
     }
 
     public void turnOffLights(){
-
+        System.out.println("turnOffLights");
     }
 }
