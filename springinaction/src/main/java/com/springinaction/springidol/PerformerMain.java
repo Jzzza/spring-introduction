0cf9b66fbd5de003214e8b0c4792bd008e34b291
Jzzza <jzzza1@gmail.com>
2020-05-16 15:42:28 +0300

    Added main for springidol and run (p. 74)

---
+++ springinaction/src/main/java/com/springinaction/springidol/PerformerMain.java
@@ -0,0 +1,13 @@
+package com.springinaction.springidol;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PerformerMain {
+    public static void main(String[] args) throws PerformerException {
+        ApplicationContext ctx =
+                new ClassPathXmlApplicationContext("spring-idol.xml");
+        Perfromer perfromer = (Perfromer) ctx.getBean("duke");
+        perfromer.perform();
+    }
+}
