8f11af0b2e1f7a44ddedd6834a3fe9f6996e67b4
Jzzza <jzzza1@gmail.com>
2020-05-16 10:55:34 +0300

    Example DI via constructor (p. 37)

--- springinaction/src/main/java/com/springinaction/knights/BraveKnight.java
+++ springinaction/src/main/java/com/springinaction/knights/BraveKnight.java
@@ -3,6 +3,7 @@ package com.springinaction.knights;
 public class BraveKnight implements Knight {
     private Quest quest;
 
+    // Пример внедрения зависмости через конструктор
     public BraveKnight(Quest quest){
         this.quest = quest; // Внедрение сценария подвига
     }
