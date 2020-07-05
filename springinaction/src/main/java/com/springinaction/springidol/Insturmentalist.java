143dc645b1886460a269fb5c4979a008c5dcea3f
Jzzza <jzzza1@gmail.com>
2020-07-05 06:47:29 +0300

    Defined Insturmentalist.java that implemented(p. 84)

---
+++ springinaction/src/main/java/com/springinaction/springidol/Insturmentalist.java
@@ -0,0 +1,25 @@
+package com.springinaction.springidol;
+
+public class Insturmentalist implements Perfromer {
+    private String song;
+    private Instrument instrument;
+
+    public Insturmentalist() {
+    }
+
+    public void perform() throws PerformerException {
+        System.out.println("Playing " + song + " : ");
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public String screamSong() {
+        return song;
+    }
+
+    public void setInstrument(Instrument instrument) {
+        this.instrument = instrument;
+    }
+}
