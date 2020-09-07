5f7e4f87a1640cfe0f23f550181a15b10cdf2717
Jzzza <jzzza1@gmail.com>
2020-09-07 23:18:26 +0300

    Add Contact for user editor example (p. 137)

---
+++ springinaction/src/main/java/com/springinaction/chapter3/propeditor/Contact.java
@@ -0,0 +1,9 @@
+package com.springinaction.chapter3.propeditor;
+
+public class Contact {
+    private PhoneNumber phoneNumber;
+
+    public void setPhoneNumber(PhoneNumber phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
