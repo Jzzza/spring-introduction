44ce137358f9bc40d2a64ccfd2accb26f270507d
Jzzza <jzzza1@gmail.com>
2020-09-07 23:17:54 +0300

    Add PhoneNumber for user editor example (p. 137)

---
+++ springinaction/src/main/java/com/springinaction/chapter3/propeditor/PhoneNumber.java
@@ -0,0 +1,16 @@
+package com.springinaction.chapter3.propeditor;
+
+public class PhoneNumber {
+    private String areaCode;
+    private String prefix;
+    private String number;
+
+    public PhoneNumber() {
+    }
+
+    public PhoneNumber(String areaCode, String prefix, String number) {
+        this.areaCode = areaCode;
+        this.prefix = prefix;
+        this.number = number;
+    }
+}
