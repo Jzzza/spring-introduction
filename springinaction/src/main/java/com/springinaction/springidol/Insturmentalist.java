978ec7c762fdbf39e2fd37d0363c033ad45a3c51
Jzzza <jzzza1@gmail.com>
2020-09-11 23:44:20 +0300

    Add example resolving dependencies for autowiring (p. 180)

--- springinaction/src/main/java/com/springinaction/springidol/Insturmentalist.java
+++ springinaction/src/main/java/com/springinaction/springidol/Insturmentalist.java
@@ -2,6 +2,7 @@ package com.springinaction.springidol;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 //public abstract class Insturmentalist implements Perfromer {
 @Configurable("pianist")
@@ -9,6 +10,7 @@ public class Insturmentalist implements Perfromer {
     private String song;
 
     @Autowired(required = false)
+    @Qualifier("guitar")
     private Instrument instrument;
     private Integer age;
 
