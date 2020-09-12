bbc32d767f8c48748c688ae0b15dab856b1195ae
Jzzza <jzzza1@gmail.com>
2020-09-12 23:08:41 +0300

    Define bean in configuration class (p. 194)

--- springinaction/src/main/java/com/springinaction/springidol/SpringIdolConfig.java
+++ springinaction/src/main/java/com/springinaction/springidol/SpringIdolConfig.java
@@ -1,9 +1,14 @@
 package com.springinaction.springidol;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringIdolConfig {
     // Здесь находятся методы, определяющие компоненты
+    @Bean
+    public Perfromer duke(){
+        return new Juggler();
+    }
 }
