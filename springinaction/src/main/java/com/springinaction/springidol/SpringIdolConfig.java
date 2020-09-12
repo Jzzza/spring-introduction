a48c5b4bace5af988a1b59e511bab47b8503ab76
Jzzza <jzzza1@gmail.com>
2020-09-12 23:06:50 +0300

    Define configuration class (p. 193)

---
+++ springinaction/src/main/java/com/springinaction/springidol/SpringIdolConfig.java
@@ -0,0 +1,9 @@
+package com.springinaction.springidol;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringIdolConfig {
+    // Здесь находятся методы, определяющие компоненты
+}
