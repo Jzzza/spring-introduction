5a5038350b280e9da898a89ed7321b7d944c2c1d
Jzzza <jzzza1@gmail.com>
2020-09-15 02:56:56 +0300

    Turn Magician from class to aspect (p. 226)

--- springinaction/src/main/java/com/springinaction/springidol/Magician.java
+++ springinaction/src/main/java/com/springinaction/springidol/Magician.java
@@ -1,10 +1,22 @@
 package com.springinaction.springidol;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
 public class Magician implements Perfromer, MindReader {
     private String magicWords;
     private MagicBox magicBox;
     private String thoughts;
     public Magician(){}
+
+    // Объявление параметризированного среза множества точек сопряжения
+    @Pointcut(
+            "execution(* com.springinaction.springidol.)" + "Thinker.thinkOfSomethings(String)) && args(thoughts)"
+    )
+    public void thinking(String thoughts) {
+    }
 
     public void perform() throws PerformerException{
         System.out.println(magicWords);
@@ -20,6 +32,7 @@ public class Magician implements Perfromer, MindReader {
         this.magicBox = magicBox;
     }
 
+    @Before("thinking(thoughts)")   // Передача параметра в совет
     public void interceptThoughts(String thoughts) {
         System.out.println("Intercepting volunteer's thoughts");
         this.thoughts = thoughts;
