6a4d20c74cbdd99f427c1537be8a07fea2f65bc1
Jzzza <jzzza1@gmail.com>
2020-09-15 02:39:17 +0300

    Create Aspect in Java style via annotations (p. 225)

--- springinaction/src/main/java/com/springinaction/springidol/Audience.java
+++ springinaction/src/main/java/com/springinaction/springidol/Audience.java
@@ -1,20 +1,33 @@
 package com.springinaction.springidol;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 
+@Aspect // Указывает, что Audience не обычный класс, а аспект
 public class Audience {
+    @Pointcut(  // Определение среза
+            "execuion(* com.springinaction.springidol.Performer.perform(..))"
+    )   // Определение среза множества точек сопряжения
+    public void performance(){
+    }
+
+    // Определение советов
+    @Before("performance()") // Выполняется перед вызовом целевого метода
     public void takeSeats() {  // Перед выступлением
         System.out.println("The audience is taking their seats.");
     }
 
+    @Before("performance()") // Выполняется перед вызовом целевого метода
     public void turnOffCellPhones() { // Перед выступлением
         System.out.println("The audience is turning off their cellphones");
     }
 
+    @AfterReturning("performance()")  // Выполняется в случае благополучного завершения следующего метода
     public void applaud() {  // После выступления
         System.out.println("CLAP CLAP CLAP CLAP CLAP");
     }
 
+    @AfterThrowing("performance()")  // Выполняется в случае появления исключения
     public void demandRefund() { // После неудачного выступления
         System.out.println("Boo! We want our money back!");
     }
