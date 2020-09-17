9b2b86d0956da837a56bb7b42f6b100b0014974d
Jzzza <jzzza1@gmail.com>
2020-09-18 00:03:55 +0300

    Extended JdbcSpitterDAO wtih SimpleJdbcdaoSupport (p. 264)

--- springinaction/src/main/java/com/springinaction/dataservice/JdbcSpitterDAO.java
+++ springinaction/src/main/java/com/springinaction/dataservice/JdbcSpitterDAO.java
@@ -1,22 +1,17 @@
 package com.springinaction.dataservice;
 
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class JdbcSpitterDAO implements SpitterDAO{
+public class JdbcSpitterDAO extends SimpleJdbcDaoSupport implements SpitterDAO{
     private static final String SQL_INSERT_SPITTER = "insert into spitter (username, password, fullname) " +
             "values (:username, :password, :fullname)";
     private static final String SQL_SELECT_SPITTER_BY_ID = "select";
-    private SimpleJdbcTemplate jdbcTemplate;
-
-    public void setJdbcTemplate(SimpleJdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     public void addSpiiter(Spitter spitter) {
         Map<String, Object> params = new HashMap<String, Object>();
@@ -24,7 +19,7 @@ public class JdbcSpitterDAO implements SpitterDAO{
         params.put("password", spitter.getPassword());
         params.put("fullname", spitter.getFullName());
 
-        jdbcTemplate.update(SQL_INSERT_SPITTER, params);
+        getSimpleJdbcTemplate().update(SQL_INSERT_SPITTER, params);
         spitter.setId(queryForIdentity());
     }
 
@@ -33,7 +28,7 @@ public class JdbcSpitterDAO implements SpitterDAO{
     }
 
     public Spitter getSpitterById(long id) {
-        return jdbcTemplate.queryForObject(SQL_SELECT_SPITTER_BY_ID, // Запрос на получение данных
+        return getSimpleJdbcTemplate().queryForObject(SQL_SELECT_SPITTER_BY_ID, // Запрос на получение данных
                 new ParameterizedRowMapper<Spitter>() {
                     public Spitter mapRow(ResultSet resultSet, int i) throws SQLException {
                         Spitter spitter = new Spitter();  // Отображение
