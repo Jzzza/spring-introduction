fc1ffe285f02e6ae3c3ce5c8683b25d1511fe976
Jzzza <jzzza1@gmail.com>
2020-09-17 23:48:56 +0300

    Add method addPitter based on SimpleJdbcTemplate (p. 260)

--- springinaction/src/main/java/com/springinaction/dataservice/JdbcSpitterDAO.java
+++ springinaction/src/main/java/com/springinaction/dataservice/JdbcSpitterDAO.java
@@ -3,9 +3,19 @@ package com.springinaction.dataservice;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
 public class JdbcSpitterDAO implements SpitterDAO{
+    private static final String SQL_INSERT_SPITTER = "update";
     private SimpleJdbcTemplate jdbcTemplate;
 
     public void setJdbcTemplate(SimpleJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void addSpiiter(Spitter spitter) {
+        jdbcTemplate.update(SQL_INSERT_SPITTER,
+                spitter.getUsername(),
+                spitter.getPassword(),
+                spitter.getFullName(),
+                spitter.isUpdatedByEmail());
+        spitter.isUpdatedByEmail();
     }
 }
