f1d506fce343b9a1d2cda0beabe8e5fe5f6ad062
Jzzza <jzzza1@gmail.com>
2020-09-19 00:05:45 +0300

    Add class for example (p. 272)

---
+++ first/src/main/java/com/habuma/spitter/domain/Spittle.java
@@ -0,0 +1,54 @@
+package com.habuma.spitter.domain;
+
+public class Spittle {
+    private long id;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private boolean updatedByEmail;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isUpdatedByEmail() {
+        return updatedByEmail;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
