d07a367f4fe3d63e029039cf75c606e88a4b8486
Jzzza <jzzza1@gmail.com>
2020-09-20 00:14:58 +0300

    Object JPA DAO without using Spring templates (p. 280)

---
+++ first/src/main/java/com/habuma/spitter/persistence/JpaSpitterDao.java
@@ -0,0 +1,34 @@
+package com.habuma.spitter.persistence;
+
+import com.habuma.spitter.domain.Spitter;
+import com.habuma.spitter.domain.SpitterDAO;
+import com.habuma.spitter.domain.Spittle;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository("spitterDao")
+@Transactional
+public class JpaSpitterDao implements SpitterDAO {
+    private static final String RECENT_SPITTLES = "SELECT s from Spittle s";
+    private static final String ALL_SPITTERS = "SELECT s FROM Spitter s";
+    private static final String SPITTER_FOR_USERNAME = "SELECT s FROM Spitter s WHERE s.username = :username";
+    private static final String SPITTLES_BY_USERNAME = "SELECT s FROM Spittle s WHERE s.spitter.username = :username";
+
+    @PersistenceContext                                             // Явно должно быть внедрено в EntityManager
+    private EntityManager em;                                       // Для внедрения EntityManager
+
+    public void addSpitter(Spitter spitter) {
+        em.persist(spitter);                                        // Использование EntityManager
+    }
+
+    public Spitter getSpitterById(long id){
+        return em.find(Spitter.class, id);                          // Использование EntityManager
+    }
+
+    public void saveSpitter(Spitter spitter){
+        em.merge(spitter);                                          // Использование EntityManager
+    }
+}
