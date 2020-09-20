56f9f5752514d645573241634887df2868c78149
Jzzza <jzzza1@gmail.com>
2020-09-21 01:42:33 +0300

    Add annotation for caching method result (p. 292)

--- first/src/main/java/com/roadrantz/dao/HibernateRantDao.java
+++ first/src/main/java/com/roadrantz/dao/HibernateRantDao.java
@@ -2,6 +2,7 @@ package com.roadrantz.dao;
 
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springmodules.cache.annotations.Cacheable;
 
 import java.util.Date;
 import java.util.List;
@@ -9,6 +10,8 @@ import java.util.List;
 public class HibernateRantDao extends HibernateDaoSupport {
     private String RANT;
 
+    // Cacheable отмечает метод, возвращаемое значение которого должно кешироваться
+    @Cacheable(modelId = "rantzCacheModel")
     public List<Rant> getRantsForDay(Date day){
         return getHibernateTemplate().find("from " + RANT +
                 " where postedDate = ?", day);
