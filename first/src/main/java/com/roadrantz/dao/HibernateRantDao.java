7d58cbdc9df71a320c379f1d2dc955c88deb0d2d
Jzzza <jzzza1@gmail.com>
2020-09-21 01:44:44 +0300

    Add annotation for caching cleaning (p. 293)

--- first/src/main/java/com/roadrantz/dao/HibernateRantDao.java
+++ first/src/main/java/com/roadrantz/dao/HibernateRantDao.java
@@ -2,6 +2,7 @@ package com.roadrantz.dao;
 
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springmodules.cache.annotations.CacheFlush;
 import org.springmodules.cache.annotations.Cacheable;
 
 import java.util.Date;
@@ -15,5 +16,11 @@ public class HibernateRantDao extends HibernateDaoSupport {
     public List<Rant> getRantsForDay(Date day){
         return getHibernateTemplate().find("from " + RANT +
                 " where postedDate = ?", day);
+    }
+
+    // При вызове метода происходит отчистка кеша
+    @CacheFlush(modelId = "rantzFlushModel")
+    public void saveRant(Rant rant) {
+        getHibernateTemplate().saveOrUpdate(rant);
     }
 }
