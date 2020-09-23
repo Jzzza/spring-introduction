85ba22be20c07fea39bd5c9bbc2b22aaa23c54b8
Jzzza <jzzza1@gmail.com>
2020-09-23 23:26:52 +0300

    Add configuration SpitterServiceImpl via annotations (p. 319)

--- first/src/main/java/com/habuma/spitter/service/SpitterServiceImpl.java
+++ first/src/main/java/com/habuma/spitter/service/SpitterServiceImpl.java
@@ -3,14 +3,18 @@ package com.habuma.spitter.service;
 import com.habuma.spitter.domain.Spitter;
 import com.habuma.spitter.persistence.JpaSpitterDao;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class SpitterServiceImpl {
     JpaSpitterDao spitterDao;
     TransactionTemplate txTemplate;
     TransactionStatus txStatus;
 
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public void saveSpitter(final Spitter spitter) {
         // Чтобы исользовать класс TransactionTemplate нужно реализовать TransactionCallback
         txTemplate.execute(new TransactionCallback<Void>(){
