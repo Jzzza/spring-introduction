4c7fef84e4f0c72c523d031b08bdeffa99bfe77d
Jzzza <jzzza1@gmail.com>
2020-09-27 23:59:46 +0300

    Added controller method for show profile with PathVariable (p. 355)

--- first/src/main/java/com/habuma/spitter/mvc/SpitterController.java
+++ first/src/main/java/com/habuma/spitter/mvc/SpitterController.java
@@ -5,6 +5,7 @@ import com.habuma.spitter.service.SpitterService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +24,7 @@ public class SpitterController {
     }
 
     @RequestMapping(value = "/spittles", method = RequestMethod.GET)
-    public String listSpittles(@RequestParam("spitter") String username, Model model){
+    public String listSpittles(@RequestParam("spitter") String username, Model model) {
         Spitter spitter = spitterService.getSpitter(username);
         model.addAttribute(spitter);
         model.addAttribute(spitterService.getSpittlesForSpitter(username));
@@ -45,5 +46,12 @@ public class SpitterController {
         spitterService.saveSpitter(spitter);
         // Переадресовать после запроса POST
         return "redirect:/spitters/" + spitter.getUsername();
+    }
+
+    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
+    public String showSpitterProfile(@PathVariable String username,
+                                     Model model) {
+        model.addAttribute(spitterService.getSpitter(username));
+        return "spitters/view";
     }
 }
