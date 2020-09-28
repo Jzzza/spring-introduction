8273cb0d89bcc0fd6fba01fc7df402da028ab10e
Jzzza <jzzza1@gmail.com>
2020-09-28 23:48:12 +0300

    Implemented file storage in AWS S3 (p. 366)

--- first/src/main/java/com/habuma/spitter/mvc/SpitterController.java
+++ first/src/main/java/com/habuma/spitter/mvc/SpitterController.java
@@ -3,6 +3,15 @@ package com.habuma.spitter.mvc;
 import com.habuma.spitter.domain.Spitter;
 import com.habuma.spitter.service.SpitterService;
 import org.apache.commons.io.FileUtils;
+import org.jets3t.service.S3Service;
+import org.jets3t.service.S3ServiceException;
+import org.jets3t.service.acl.AccessControlList;
+import org.jets3t.service.acl.GroupGrantee;
+import org.jets3t.service.acl.Permission;
+import org.jets3t.service.impl.rest.httpclient.RestS3Service;
+import org.jets3t.service.model.S3Bucket;
+import org.jets3t.service.model.S3Object;
+import org.jets3t.service.security.AWSCredentials;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 
@@ -22,6 +32,8 @@ import java.io.IOException;
 public class SpitterController {
     private final SpitterService spitterService;
     private String webRootPath="";
+    private String s3SecretKey;
+    private String s3AccessKey;
 
     @Inject
     public SpitterController(SpitterService spitterService) {
@@ -66,9 +78,27 @@ public class SpitterController {
 
     private void saveImage(String filename, MultipartFile image) throws ImageUploadException{
         try{
-            File file = new File(webRootPath + "/resources/" + filename);
-            FileUtils.writeByteArrayToFile(file, image.getBytes());
-        } catch (IOException e) {
+            AWSCredentials awsCredentials = new AWSCredentials(s3AccessKey, s3SecretKey);
+            S3Service s3 = new RestS3Service(awsCredentials); // Настройка S3
+
+            // Создать объекты, представляющие хранилище и изображения
+            S3Bucket imageBucket = s3.getBucket("spitterImages");
+            S3Object imageObject = new S3Object(filename);
+
+            // Скопировать данные изображения в объект
+            imageObject.setDataInputStream(new ByteArrayInputStream(image.getBytes()));
+            imageObject.setContentLength(image.getBytes().length);
+            imageObject.setContentType("image/jpeg");
+
+            // Определить разрешение
+            AccessControlList acl = new AccessControlList();
+            acl.setOwner(imageBucket.getOwner());
+            acl.grantPermission(GroupGrantee.ALL_USERS, Permission.PERMISSION_READ);
+            imageObject.setAcl(acl);
+
+            s3.putObject(imageBucket, imageObject);
+
+        } catch (Exception e) {
             throw new ImageUploadException("Unable to save image", e);
         }
     }
