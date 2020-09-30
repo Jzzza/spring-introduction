73a5d62ce6c1d9a829f3965640ec86998e1b8302
Jzzza <jzzza1@gmail.com>
2020-09-30 03:27:53 +0300

    Return view for home (p. 372)

---
+++ first/src/main/java/com/roadrantz/dao/HomeController.java
@@ -0,0 +1,20 @@
+package com.roadrantz.dao;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    private List<Rant> recentRants=new ArrayList<Rant>();
+
+    @RequestMapping({"/", "home"})
+    public ModelAndView showHomePage(Map<String, Object> recentRants) {
+        return new ModelAndView("home", "rants" , recentRants);
+    }
+}
