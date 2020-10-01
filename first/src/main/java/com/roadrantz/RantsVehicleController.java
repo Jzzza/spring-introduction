318a8a028ff0f2998910a8ce6ec56aad4471b46b
Jzzza <jzzza1@gmail.com>
2020-10-01 23:26:57 +0300

    Change handle method (p. 387)

--- first/src/main/java/com/roadrantz/RantsVehicleController.java
+++ first/src/main/java/com/roadrantz/RantsVehicleController.java
@@ -1,8 +1,14 @@
 package com.roadrantz;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import sun.plugin2.main.client.WMozillaServiceDelegate;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.BindException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class RantsVehicleController {
@@ -15,5 +21,14 @@ public class RantsVehicleController {
             extension="";
         }
         return BASE_BIEW_NAME + extension;
+    }
+
+    protected ModelAndView handle(HttpServletRequest request,
+                                  HttpServletResponse response,
+                                  Object command,
+                                  BindException errors) throws Exception{
+        Map<String, Object> model=new HashMap<String, Object>();
+        return new ModelAndView(getViewName(request), model);
+
     }
 }
